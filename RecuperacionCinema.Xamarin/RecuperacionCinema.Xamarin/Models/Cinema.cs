6807b1c141ee9866a31e66bd09b850813f2e8658
FabianDesarrollador <desarrolladorsistemasfabian@gmail.com>
2018-10-28 15:51:06 -0500

    Creacion clase pelicula y funciones

--- RecuperacionCinema.Xamarin/RecuperacionCinema.Xamarin/Models/Cinema.cs
+++ RecuperacionCinema.Xamarin/RecuperacionCinema.Xamarin/Models/Cinema.cs
@@ -6,5 +6,23 @@ namespace RecuperacionCinema.Xamarin.Models
 {
     class Cinema
     {
+        public class Pelicula
+        {
+            public string Nombre { get; set; }
+            public DateTime FechaEstreno { get; set; }
+            public string Genero { get; set; }
+            public string Recomendacion { get; set; }
+            public int Duracion { get; set; }
+            public string Imagen { get; set; }
+            public Funciones[] Funciones { get; set; }
+        }
+
+        public class Funciones
+        {
+            public string Cinema { get; set; }
+            public string Sala { get; set; }
+            public string Ciudad { get; set; }
+            public int Precio { get; set; }
+        }
     }
 }
