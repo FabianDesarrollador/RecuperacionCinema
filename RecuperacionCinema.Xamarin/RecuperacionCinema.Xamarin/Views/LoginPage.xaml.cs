6fca01fa3ecb35acbd3faabeabb67ee53ee45005
FabianDesarrollador <desarrolladorsistemasfabian@gmail.com>
2018-10-28 15:58:53 -0500

    Eliminamos Mainpage, Creacion de la navigacion desde el APP.XML, Viesta y Codigo de LoginPage

--- RecuperacionCinema.Xamarin/RecuperacionCinema.Xamarin/Views/LoginPage.xaml.cs
+++ RecuperacionCinema.Xamarin/RecuperacionCinema.Xamarin/Views/LoginPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,44 @@ namespace RecuperacionCinema.Xamarin.Views
 		public LoginPage ()
 		{
 			InitializeComponent ();
+            loginboton.Clicked += loginboton_Clicked;
 		}
-	}
+
+        private async void loginboton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(userEntry.Text))
+            {
+                await DisplayAlert("Error", "Debe Ingresar un Usuario", "Aceptar");
+                userEntry.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(passwordEntry.Text))
+            {
+                await DisplayAlert("Error", "Debe Ingresar un Usuario", "Aceptar");
+                passwordEntry.Focus();
+                return;
+            }
+
+            loginActivity.IsRunning = true;
+            loginboton.IsEnabled = false;
+            HttpClient cliente = new HttpClient();
+            cliente.BaseAddress = new Uri("https://misapis.azurewebsites.net");
+            string url = string.Format("/api/Seguridad/{0}/{1}", userEntry.Text, passwordEntry.Text);
+            var response = await cliente.GetAsync(url);
+            var resultado = response.Content.ReadAsStringAsync().Result;
+            loginboton.IsEnabled = true;
+            loginActivity.IsRunning = false;
+
+            if (string.IsNullOrEmpty(resultado) || resultado == "null")
+            {
+                await DisplayAlert("Error", "Usuario o clave no válidos", "Aceptar");
+                passwordEntry.Text = string.Empty;
+                passwordEntry.Focus();
+                return;
+            }
+
+            await Navigation.PushModalAsync(new CarteleraPage());
+        }
+    }
 }
