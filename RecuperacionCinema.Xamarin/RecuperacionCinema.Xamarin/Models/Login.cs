e9674b61fe6a99ee82e9de92875f0121add08ae3
FabianDesarrollador <desarrolladorsistemasfabian@gmail.com>
2018-10-28 15:49:08 -0500

    Creacion Carpeta Models con las dos clases(Login y Cinema), Carpeta Vistas con todas las vistas

---
+++ RecuperacionCinema.Xamarin/RecuperacionCinema.Xamarin/Models/Login.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecuperacionCinema.Xamarin.Models
+{
+    class Login
+    {
+    }
+}
---
+++ RecuperacionCinema.Xamarin/RecuperacionCinema.Xamarin/Views/ResumenPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RecuperacionCinema.Xamarin.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ResumenPage : ContentPage
+	{
+		public ResumenPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
